48612ea4be0c0acbcc6636e2ab670198e04bab8a
Aduda-Boaz <adudaboaz@gmail.com>
2021-06-15 20:33:30 +0300

    Select from nobel modified

--- select_from_nobel.sql
+++ select_from_nobel.sql
@@ -55,4 +55,19 @@ SELECT *
   FROM nobel 
 WHERE winner = 'PETER GRÜNBERG';
 
+-- Find all details of the prize won by EUGENE O'NEILL
+SELECT *
+  FROM nobel
+WHERE winner = 'EUGENE O''NEILL';
 
+-- List the winners, year and subject where the winner starts with Sir. Show the the most recent first, then by name order.
+SELECT winner, yr, subject 
+  FROM nobel 
+  WHERE winner LIKE 'Sir%'
+ORDER BY yr DESC, winner;
+
+-- Show the 1984 winners and subject ordered by subject and winner name; but list Chemistry and Physics last.
+SELECT winner, subject
+  FROM nobel
+  WHERE yr = 1984
+ORDER BY subject IN('Physics', 'Chemistry'), subject, winner;
